b0d0cdebcdd33b07ae05281988fbb83f3169dbd5
d0loreshaze <69720260+d0loreshaze@users.noreply.github.com>
2020-08-17 04:40:13 +0000

    Added some more tools to my initial setup

--- kali_rc_scripts/setup.sh
+++ kali_rc_scripts/setup.sh
@@ -1,5 +1,14 @@
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install apktool
 sudo apt-get install shellter
 sudo apt-get install python3-pip
 sudo apt-get install zipalign
 sudo apt-get install remmina
+sudo apt-get install git
+git clone https://github.com/sherlock-project/sherlock.git
+cd sherlock && python3 -m pip install -r requirements.txt && cd ..
+git clone https://github.com/threat9/routersploit.git
+cd routersploit && python3 -m pip install -r requirements.txt && cd ..
+git clone https://github.com/Z4nzu/hackingtool.git
+cd hackingtool && python3 -m pip install -r requirements.txt && cd ..
